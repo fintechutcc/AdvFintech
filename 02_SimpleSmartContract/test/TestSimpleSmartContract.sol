9355854e24cf998f5603978590a8e4f9275487cf
Suparerk Manitpornsut <suparerk_man@utcc.ac.th>
2021-06-30 14:30:36 +0700

    Minor updates.

--- 02_SimpleSmartContract/test/TestSimpleSmartContract.sol
+++ 02_SimpleSmartContract/test/TestSimpleSmartContract.sol
@@ -8,8 +8,17 @@ contract TestSimpleSmartContract {
   // Create simplesmartContract 
   SimpleSmartContract simpleSmartContract = SimpleSmartContract(DeployedAddresses.SimpleSmartContract());
 
+  // Test get function
   function testGet() public {
     uint expectedMyVariable = 0;  
     Assert.equal(expectedMyVariable, simpleSmartContract.get(), "Testing on get() function. Expected to return 0.");
+  }
+
+  // Test set function
+  function testSet() public {
+    uint expectedMyVariable = 2021;
+
+    simpleSmartContract.set(expectedMyVariable);
+    Assert.equal(expectedMyVariable, simpleSmartContract.get(), "Testing on set() function. Expected to set myVariable to 2021.");
   }
 }
